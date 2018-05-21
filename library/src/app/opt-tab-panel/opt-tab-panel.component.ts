68d855fea81f440f72eca2c0815809ff306480f2
Sasanika <hkasasanika@gmail.com>
2018-05-21 18:09:04 +0530

    tab panel

---
+++ library/src/app/opt-tab-panel/opt-tab-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opt-tab-panel',
+  templateUrl: './opt-tab-panel.component.html',
+  styleUrls: ['./opt-tab-panel.component.scss']
+})
+export class OptTabPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
