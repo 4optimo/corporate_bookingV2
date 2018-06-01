42108c379429d0c38c01a21f5c743ddeb08e8432
Sasanika <hkasasanika@gmail.com>
2018-06-01 10:40:21 +0530

    booking list expand ts

---
+++ library/src/app/opt-booking-list-expand/opt-booking-list-expand.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatExpansionPanel} from "@angular/material";
+
+@Component({
+  selector: 'app-opt-booking-list-expand',
+  templateUrl: './opt-booking-list-expand.component.html',
+  styleUrls: ['./opt-booking-list-expand.component.scss']
+})
+export class OptBookingListExpandComponent implements OnInit {
+
+  @ViewChild('myPanel1') myPanel1: MatExpansionPanel;
+  matIcon1 = 'keyboard_arrow_down' || 'keyboard_arrow_up';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.myPanel1.expandedChange.subscribe((data) => {
+      this.matIcon1 = data ? 'keyboard_arrow_up' : 'keyboard_arrow_down';
+    });
+  }
+  expandPannel() {
+    this.myPanel1.expanded = !this.myPanel1.expanded;
+    // alert("1");
+  }
+}
