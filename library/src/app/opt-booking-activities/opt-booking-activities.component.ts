1396688a47cfb49cf250eb4b592263e1a086ab89
Administrator <djay23.work@gmail.com>
2018-06-08 18:37:43 +0530

    Booking Activities Component - Added - Added Expansion Panel - Added Stepper - Changed Expansion Panel Styles

---
+++ library/src/app/opt-booking-activities/opt-booking-activities.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opt-booking-activities',
+  templateUrl: './opt-booking-activities.component.html',
+  styleUrls: ['./opt-booking-activities.component.scss']
+})
+export class OptBookingActivitiesComponent implements OnInit {
+  // isLinear = false;
+  // firstFormGroup: FormGroup;
+  // secondFormGroup: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    // this.firstFormGroup = this._formBuilder.group({
+    //   firstCtrl: ['', Validators.required]
+    // });
+    // this.secondFormGroup = this._formBuilder.group({
+    //   secondCtrl: ['', Validators.required]
+    // });
+  }
+
+}
