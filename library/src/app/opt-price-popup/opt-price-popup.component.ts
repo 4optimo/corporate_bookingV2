253a2bd263e7da4b29d60f4eaa66df9fa872dd9a
Sasanika <hkasasanika@gmail.com>
2018-07-13 10:28:26 +0530

    updated

---
+++ library/src/app/opt-price-popup/opt-price-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opt-price-popup',
+  templateUrl: './opt-price-popup.component.html',
+  styleUrls: ['./opt-price-popup.component.scss']
+})
+export class OptPricePopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
