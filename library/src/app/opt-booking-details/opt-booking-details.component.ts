7ae8bdc696015452a5f85a16e68ee61ea3877f47
4optimo <mohan.ranasinghe@programus.co.uk>
2018-07-18 15:16:35 +0530

    modal

--- library/src/app/opt-booking-details/opt-booking-details.component.ts
+++ library/src/app/opt-booking-details/opt-booking-details.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild, ViewChildren} from '@angular/core';
 import {MatExpansionPanel} from "@angular/material";
-
+import {FormControl} from '@angular/forms';
 @Component({
   selector: 'app-opt-booking-details',
   templateUrl: './opt-booking-details.component.html',
@@ -11,6 +11,9 @@ export class OptBookingDetailsComponent implements OnInit {
 
 
 
+
+  dater = new FormControl(new Date());
+  serializedDater = new FormControl((new Date()).toISOString());
 
   @ViewChild('myPanel') myPanel: MatExpansionPanel;
   matIcon = 'keyboard_arrow_down' || 'keyboard_arrow_up';
@@ -80,6 +83,7 @@ export class OptBookingDetailsComponent implements OnInit {
       "name": "BTX16L4 - Premier Dinner 4 & Eye"
     }
   ];
+
 
 
 }
