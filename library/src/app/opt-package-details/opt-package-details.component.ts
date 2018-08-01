05b9c2e5d9a17068d41f890c3d2f64bd8dd6cda9
Sasanika <hkasasanika@gmail.com>
2018-08-01 10:48:16 +0530

    updated

--- library/src/app/opt-package-details/opt-package-details.component.ts
+++ library/src/app/opt-package-details/opt-package-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatExpansionPanel} from "@angular/material";
 
 @Component({
   selector: 'app-opt-package-details',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OptPackageDetailsComponent implements OnInit {
 
+  @ViewChild('myPanel') myPanel: MatExpansionPanel;
+  matIcon = 'keyboard_arrow_down' || 'keyboard_arrow_up';
+
   constructor() { }
 
   ngOnInit() {
+    this.myPanel.expandedChange.subscribe((data) => {
+      this.matIcon = data ? 'keyboard_arrow_up' : 'keyboard_arrow_down';
+    });
+
+  }
+  expandPannel() {
+    this.myPanel.expanded = !this.myPanel.expanded;
+    // alert("1");
   }
 
 }
