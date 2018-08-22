786f3008861fc7d2535f86245d8bb27c4b4b05cd
Kithmini <dulanga.kitty21@gmail.com>
2018-08-22 18:55:50 +0530

    Resloved responsive Issues

--- library/src/app/opt-booking-activities/opt-booking-activities.component.ts
+++ library/src/app/opt-booking-activities/opt-booking-activities.component.ts
@@ -1,15 +1,30 @@
-import { Component} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ModalContentComponent} from "../modal-content/modal-content.component";
+import {MatExpansionPanel} from "@angular/material";
 
 @Component({
   selector: 'app-opt-booking-activities',
   templateUrl: './opt-booking-activities.component.html',
   styleUrls: ['./opt-booking-activities.component.scss']
 })
-export class OptBookingActivitiesComponent {
+export class OptBookingActivitiesComponent implements OnInit{
   foods = [
     {value: 'steak-0', viewValue: 'All'},
     {value: 'pizza-1', viewValue: 'Pizza'},
     {value: 'tacos-2', viewValue: 'Tacos'}
   ];
+
+  @ViewChild('myPanel1') myPanel1: MatExpansionPanel;
+  matIcon1 = 'keyboard_arrow_down' || 'keyboard_arrow_up';
+
+
+  ngOnInit() {
+    this.myPanel1.expandedChange.subscribe((data) => {
+      this.matIcon1 = data ? 'keyboard_arrow_up' : 'keyboard_arrow_down';
+    });
+  }
+  expandPannel() {
+    this.myPanel1.expanded = !this.myPanel1.expanded;
+    // alert("1");
+  }
 }
