24fd5b7fc0b71e8cbabd27ce796fdcce3a570909
Hazeer <hazeerahamed@gmail.com>
2018-09-04 18:11:40 +0530

    Component library updates

--- library/src/app/opt-reprint-popup/opt-reprint-popup.component.ts
+++ library/src/app/opt-reprint-popup/opt-reprint-popup.component.ts
@@ -1,6 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject, OnInit, Input} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {ModalContentComponent} from "../modal-content/modal-content.component";
+import {MatExpansionModule, MatExpansionPanel} from '@angular/material/expansion';
 
 @Component({
   selector: 'app-opt-reprint-popup',
@@ -8,6 +9,9 @@ import {ModalContentComponent} from "../modal-content/modal-content.component";
   styleUrls: ['./opt-reprint-popup.component.scss']
 })
 export class OptReprintPopupComponent implements OnInit {
+   @Input() collapsedHeight= "55px";
+   @Input() expandedHeight: "55px";
+   panel: MatExpansionPanel
 
   constructor(
     public  matDialogRef: MatDialogRef<ModalContentComponent>,
